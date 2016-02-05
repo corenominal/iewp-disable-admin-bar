a1b811ec314a14915c3f62b638d4e3a20531c309
Philip Newborough <corenominal@corenominal.org>
2016-02-05 15:26:50 +0000

    Initial import

---
+++ iewp-disable-admin-bar.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: IEWP Disable Admin Bar
+ * Plugin URI: https://github.com/corenominal/iewp-disable-admin-bar
+ * Description: A single purpose WordPress plugin, disables the admin bar for all users.
+ * Author: Philip Newborough
+ * Version: 0.0.1
+ * Author URI: https://corenominal.org
+ */
+
+show_admin_bar( false );
